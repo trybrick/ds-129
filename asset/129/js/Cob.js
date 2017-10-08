dcbeafeb978f07d8bcd9e22787ef3c869e43bc88
Tom Noogen <tom@noogen.net>
2017-10-08 15:24:59 -0500

    scrollspy does not exists

--- asset/129/js/Cob.js
+++ asset/129/js/Cob.js
@@ -1,18 +1,19 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function () {
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
+/*$('body').scrollspy({
     target: '.navbar-fixed-top'
 })
+*/
 
 // Closes the Responsive Menu on Menu Item Click
 /*
